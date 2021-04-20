fd7c1b12f373f1bb675814c82397efad5c5f4336
riCx12 <victor.r.rici@gmail.com>
2021-04-20 18:29:58 -0300

    Inclusão de novos conceitos de arraylist

---
+++ src/main/kotlin/one/digitalinnovation/collections/TesteList.kt
@@ -0,0 +1,28 @@
+package one.digitalinnovation.collections
+
+fun main() {
+    val joao = Funcionario ( "Joao", 2000.0, "CLT")
+    val pedro = Funcionario ( "Pedro", 1500.0, "CLT")
+    val maria = Funcionario ( "Maria", 4000.0, "PJ")
+
+    val funcionarios = listOf(joao, pedro, maria)
+    funcionarios.forEach { println(it) }
+    println(funcionarios.find{it.nome == "Maria"})
+
+    println("-------------------")
+    funcionarios.groupBy{ it.tipo}.forEach { println(it) }
+
+
+}
+
+data class Funcionario(
+    val nome: String,
+    val salario: Double,
+    val tipo: String
+) {
+    override fun toString(): String =
+        """
+     Nome: $nome
+     Salario: $salario
+""".trimIndent()
+}
